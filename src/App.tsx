318720138de0ff755ee9fc325d7f8cb2088ad0e2
Marcos <sebastianimfeld@gmail.com>
2026-03-16 16:11:48 -0300

    adjustment for multidevice and rotation refresh token

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
 // components
-import Login from "./components/auth/Login"
-import Home from "./components/common/Home"
-import ProtectedRoute from "./components/auth/ProtectedRoute"
-import PersistLogin from "./components/auth/PersistLogin"
+import Login from "./components/auth/Login";
+import Home from "./components/common/Home";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import PersistLogin from "./components/auth/PersistLogin";
+import PublicRoute from "./components/auth/PublicRoute";
 
-import ProfileBase from "./components/common/permissions"
+import ProfileBase from "./components/common/permissions";
 
 function App(): JSX.Element {
   return (
     <div>
       <Routes>
-        {/* public routes */}
-        <Route path="/login" element={<Login />} />
-        {/* <Route path="/forgot-password" element={<ForgotPasswordForm />} /> */}
-        {/* <Route path="/recovery-password/:token" element={<NewPasswordForm />} /> */}
-        {/* private routes */}
         <Route element={<PersistLogin />}>
+          {/* public routes */}
+          <Route element={<PublicRoute />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+          {/* <Route path="/forgot-password" element={<ForgotPasswordForm />} /> */}
+          {/* <Route path="/recovery-password/:token" element={<NewPasswordForm />} /> */}
+          {/* private routes */}
           <Route
             element={<ProtectedRoute allowedRoles={ProfileBase.routes.view} />}
           >
@@ -26,7 +29,7 @@ function App(): JSX.Element {
         </Route>
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
